80e6610b25ec6cdbc4583b67c02a16ceb589ff55
noe.cornu <noe.cornu1@gmail.com>
2023-12-03 13:11:42 +0100

    Remove confirmation grid detection

--- Interface/_interface/interface.c
+++ Interface/_interface/interface.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+//#include "../display/display_solved.h"
 
 char *rotate_filename = ".rotate_image.png";
 char *solve_filename = "solved_grid.png";
@@ -250,62 +251,6 @@ void on_gridButton_clicked(GtkButton *button, gpointer user_data) {
     GtkWidget *imageWidget = GTK_WIDGET(user_data); 
 
     printf("Grid button clicked\n");
-
-    // Create a confirmation dialog
-    GtkWidget *dialog_grid = gtk_dialog_new_with_buttons("Grid confirmation",
-                                                         NULL,
-                                                         GTK_DIALOG_MODAL,
-                                                         "Yes",
-                                                         GTK_RESPONSE_YES,
-                                                         "Try Again",
-                                                         GTK_RESPONSE_NO,
-                                                         NULL); // NULL sentinel
-
-    // Set the default size of the dialog
-    gtk_window_set_default_size(GTK_WINDOW(dialog_grid), 200, 100);
-
-    // Set the border width (margin) of the dialog
-    gtk_container_set_border_width(GTK_CONTAINER(dialog_grid), 10);
-
-    // Create a label and add it to the dialog's content area
-    GtkWidget *label = gtk_label_new("Is the grid correctly detected?");
-    gtk_widget_set_size_request(label, 180, 100);  // Set the size of the label
-    GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog_grid));
-    gtk_container_add(GTK_CONTAINER(content_area), label);
-    gtk_widget_show(label);
-
-    // Create a box for the buttons
-    GtkWidget *button_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    gtk_widget_set_halign(button_box, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(button_box, GTK_ALIGN_CENTER);
-
-    // Add the box to the dialog's content area
-    gtk_container_add(GTK_CONTAINER(content_area), button_box);
-    gtk_widget_show(button_box);
-
-    gtk_dialog_set_default_response(GTK_DIALOG(dialog_grid), GTK_RESPONSE_YES);
-
-    gint response = gtk_dialog_run(GTK_DIALOG(dialog_grid));
-
-    if (response == GTK_RESPONSE_NO) {
-        printf("Grid is not correctly detected\n");
-        // Use the system() function to execute the grid program
-        char grid_command[256];
-        //sprintf(grid_command, "../grid/grid .rotate_image.png solved_grid.png");
-        int status = system(grid_command);
-
-        if (status != 0) {
-            // Handle errors from executing the grid program
-            g_print("Error executing the grid program.\n");
-        }
-
-        on_gridButton_clicked(button, imageWidget);
-    }
-    else{
-        printf("Grid is correctly detected\n");
-    }
-
-    gtk_widget_destroy(dialog_grid);
 }
 
 // Déclaration de la fonction on_window_delete_event
