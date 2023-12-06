fd1b67d6bf9f127a00de99bb4b79513a516a83be
noe.cornu <noe.cornu1@gmail.com>
2023-12-06 16:15:55 +0100

    display grid on user's input image

--- display/diplay_solved.c
+++ display/diplay_solved.c
@@ -83,35 +83,55 @@ int main() {
 	}
 //------End of Creation of the grid--------------------------------	
 
-    // display grid matrix
-    for (int i = 0; i < 9; i++) {
-        cout << endl;
-        for (int j = 0; j < 9; j++) {
-            cout << grid[i][j] << " ";
-        }
-    }
-    
+//------Read the coo file and asign x0, y0... variables------------
+	std::string filename = "co";
+	std::ifstream file(filename);
+	if (!file) {
+		std::cerr << "Unable to open file: " << filename << std::endl;
+		return 1;  // Return an error code
+	}
+
+	int x0, y0, x1, y1, x2, y2, x3, y3;
+	if (!(file >> x0 >> y0 >> x1 >> y1 >> x2 >> y2 >> x3 >> y3)) {
+		std::cerr << "Error: file doesn't contain 8 integers" << std::endl;
+		return 1;  // Return an error code
+	}
+
+	// Print the values
+	std::cout << x0 << '\n' << y0 << '\n' << x1 << '\n' << y1 << '\n' << x2 << '\n' << y2 << '\n' << x3 << '\n' << y3 << '\n';
+//------End of reading the coo file--------------------------------
+
 
     // Font settings for displaying digits
     int fontFace = FONT_HERSHEY_SIMPLEX;
     double fontScale = 2;
     int thickness = 3;
 
-    // Loop through each cell in the Sudoku grid and display the digit on the image
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (grid[i][j] != 0) {
-                // Convert row and column indices to pixel coordinates
-                int x = j * sudokuImage.cols / 9 -20;
-                int y = i * sudokuImage.rows / 9 +5;
+	// Assume x0, y0, x1, y1, x2, y2, x3, y3 are defined and initialized
 
-                // Display the digit on the image
-                string digit = to_string(grid[i][j]);
-                Point textOrg(x + sudokuImage.cols / 18, y + sudokuImage.rows / 14);
-                putText(sudokuImage, digit, textOrg, fontFace, fontScale, Scalar(150, 150, 150), thickness);
-            }
-        }
-    }
+	// Calculate the width and height of the grid
+	int gridWidth = x1 - x0;
+	int gridHeight = y3 - y0;
+
+	// Calculate the width and height of a cell
+	int cellWidth = gridWidth / 9;
+	int cellHeight = gridHeight / 9;
+
+	// Loop through each cell in the Sudoku grid and display the digit on the image
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (grid[i][j] != 0) {
+				// Convert row and column indices to pixel coordinates
+				int x = x0 + j * cellWidth;
+				int y = y0 + i * cellHeight;
+
+				// Display the digit on the image
+				string digit = to_string(grid[i][j]);
+				Point textOrg(x + cellWidth / 2, y + cellHeight / 2);
+				putText(sudokuImage, digit, textOrg, fontFace, fontScale, Scalar(150, 150, 150), thickness);
+			}
+		}
+	}
 
     // Display the Sudoku grid image with digits
     //namedWindow("Sudoku Grid", WINDOW_NORMAL);
