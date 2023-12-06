855e648b3aaa876c9e6822787513c467ec0edc04
noe.cornu <noe.cornu1@gmail.com>
2023-12-06 10:17:56 +0100

    from txt file to matrix bug correction

--- display/diplay_solved.c
+++ display/diplay_solved.c
@@ -6,6 +6,8 @@
 using namespace cv;
 using namespace std;
 
+typedef int Grid[9][9];
+
 int main() {
     // Load Sudoku grid image
     Mat sudokuImage = imread("sudoku_image.jpg", IMREAD_COLOR);
@@ -16,35 +18,77 @@ int main() {
         return -1;
     }
 
-    // Load Sudoku grid from text file
-    int sudokuGrid[9][9] = {0};  // Initialize all cells to 0
-    ifstream file("grid_00");
-    string line;
-    int row = 0;
+//------File lecture----------------------------------------------
+	//file content stocked in fileContent 
+	
+	FILE* fichier = NULL;
 
-    while (getline(file, line)) {
-        // Skip empty lines
-        if (line.empty()) continue;
+	fichier = fopen("grid_00", "r");
 
-        // Parse characters and convert them to integers
-        for (int col = 0; col < 9; col++) {
-            if (line[col] >= '1' && line[col] <= '9') {
-                sudokuGrid[row][col] = line[col] - '0';  // Convert char to int
-            }
+	if (fichier == NULL)//manage error if file not ok 
+	{
+		printf("ERREUR: Impossible file opening");
+		fclose(fichier);
+		return 1;
+	}
+
+	
+	char fileContent[105] = "";
+	char temp[11];
+
+	while(fgets(temp, 11, fichier) != NULL)
+	{	
+		strcat(fileContent, temp);
+	}
+
+	//printf("%s", fileContent);
+	char str[82] = "";
+	size_t k = 0;
+	size_t q = 0;
+
+	while(fileContent[k] != '\0')
+	{
+		if(fileContent[k] >= 46 && fileContent[k] <= 57)
+		{
+			str[q] = fileContent[k];
+			q++;
+		}
+		k++;
+	}
+
+	//printf("%s", str);
+
+	fclose(fichier);
+//------End of file lecture--------------------------------------
+
+//------Creation of the grid-------------------------------------
+	//from 1 to 9 on X an Y
+	//0 unused
+	
+	Grid grid;  // X Y VAL&POS
+	
+	size_t index = 0; // index for the string
+
+	for(size_t y = 0; y < 9; y++)
+	{
+		for (size_t x = 0; x < 9; x++)
+		{
+			if (str[index] == '.')
+				grid[y][x] = 0;
+
+			else
+				grid[y][x] = str[index]-48;
+			index++;
+		}
+	}
+//------End of Creation of the grid--------------------------------	
+
+    // display grid matrix
+    for (int i = 0; i < 9; i++) {
+        cout << endl;
+        for (int j = 0; j < 9; j++) {
+            cout << grid[i][j] << " ";
         }
-
-        row++;
-    }
-
-    file.close();
-
-    for (size_t i = 0; i < 9; i++)
-    {
-        for (size_t j = 0; j < 9; j++)
-        {
-            printf("%d ", sudokuGrid[i][j]);
-        }
-        printf("\n");
     }
     
 
@@ -56,13 +100,13 @@ int main() {
     // Loop through each cell in the Sudoku grid and display the digit on the image
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            if (sudokuGrid[i][j] != 0) {
+            if (grid[i][j] != 0) {
                 // Convert row and column indices to pixel coordinates
                 int x = j * sudokuImage.cols / 9 -20;
                 int y = i * sudokuImage.rows / 9 +5;
 
                 // Display the digit on the image
-                string digit = to_string(sudokuGrid[i][j]);
+                string digit = to_string(grid[i][j]);
                 Point textOrg(x + sudokuImage.cols / 18, y + sudokuImage.rows / 14);
                 putText(sudokuImage, digit, textOrg, fontFace, fontScale, Scalar(150, 150, 150), thickness);
             }
---
+++ display/display_solved.h
@@ -0,0 +1,8 @@
+// display_solved.h
+
+#ifndef DISPLAY_SOLVED_H
+#define DISPLAY_SOLVED_H
+
+void display_solved(int sudokuGrid[9][9]);
+
+#endif // DISPLAY_SOLVED_H
